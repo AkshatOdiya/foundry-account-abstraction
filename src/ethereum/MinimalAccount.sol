f5a0aa34beb4fdbed01b8703ec378e1b5e321c54
AkshatOdiya <akshatodiya79@gmail.com>
2025-08-09 17:22:40 +0530

    Added/Modified/Tests

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -7,17 +7,52 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 
+// MinimalAccount will pretend to be our EOA
 contract MinimalAccount is IAccount, Ownable {
-    constructor() Ownable(msg.sender) {}
+    error MinimalAccount__NotFromEntryPoint();
+    error MinimalAccount__NotFromEntryPointOrOwner();
+    error MinimalAccount__TransferFailed(bytes recepient);
+
+    IEntryPoint private immutable i_entryPoint;
+
+    modifier requireFromEntryPoint() {
+        if (msg.sender != address(i_entryPoint)) {
+            revert MinimalAccount__NotFromEntryPoint();
+        }
+        _;
+    }
+
+    modifier requireFromEntryPointOrOwner() {
+        if (msg.sender != address(i_entryPoint) && msg.sender != owner()) {
+            revert MinimalAccount__NotFromEntryPointOrOwner();
+        }
+        _;
+    }
+
+    constructor(address entryPoint) Ownable(msg.sender) {
+        i_entryPoint = IEntryPoint(entryPoint);
+    }
+
+    // An account should also accept funds
+    receive() external payable {}
 
     // A Signature is valid, if it's the MinimalAccount owner
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
+        requireFromEntryPoint
         returns (uint256 validationData)
     {
         validationData = _validateSignature(userOp, userOpHash);
         _payPrefund(missingAccountFunds);
+    }
+
+    function execute(address dest, uint256 value, bytes calldata funcData) external requireFromEntryPointOrOwner {
+        (bool success, bytes memory result) = dest.call{value: value}(funcData);
+        if (!success) {
+            revert MinimalAccount__TransferFailed(result);
+        }
     }
 
     function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
@@ -33,11 +68,16 @@ contract MinimalAccount is IAccount, Ownable {
         return SIG_VALIDATION_SUCCESS;
     }
 
+    // function to send funds to entryPoint as fee for preceeding
     function _payPrefund(uint256 missingAccountFunds) internal {
         // function validateUserOp can only be callable by entryPoint contract therefore, msg.sender(entryPoint contract) is payable
         if (missingAccountFunds != 0) {
             (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
             (success);
         }
+    }
+
+    function getEntryPoint() external view returns (address) {
+        return address(i_entryPoint);
     }
 }
